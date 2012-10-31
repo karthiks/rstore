25dee036f9c0fc4c11b9e404c97c7fab0d404776
Ravindra <ravi@cloudmunch.com>
2012-10-31 21:07:21 +0530

    modified db update statements to add #UP and #DOWN

--- db/scripts/01.sql
+++ db/scripts/01.sql
@@ -1,1 +1,4 @@
+#UP
 update jpetstore.item SET attr2="update1" where itemid="EST-1";
+#DOWN
+update jpetstore.item SET attr2="update0" where itemid="EST-1";
--- db/scripts/03.sql
+++ db/scripts/03.sql
@@ -1,1 +1,4 @@
+#UP
 update jpetstore.item SET attr2="update3" where itemid="EST-1";
+#DOWN
+update jpetstore.item SET attr2="update2" where itemid="EST-1";
